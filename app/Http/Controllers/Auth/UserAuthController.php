daedbf27f5fb08f824f3168a99bca768d8067703
krisnajufer <krisnajufer07@gmail.com>
2022-08-15 11:38:31 +0700

    syncronize login auth with work_schedules on auth/UserAuthController

--- app/Http/Controllers/Auth/UserAuthController.php
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -9,6 +9,9 @@ use Illuminate\Support\Facades\Validator;
 use F9Web\ApiResponseHelpers;
 use Illuminate\Http\JsonResponse;
 use App\Models\Auth\UserAuth;
+use App\Models\Admin\WorkSchedules;
+use App\Models\Admin\Employees;
+use Illuminate\Support\Carbon;
 
 class UserAuthController extends Controller
 {
@@ -33,10 +36,20 @@ class UserAuthController extends Controller
                 return $this->respondUnAuthenticated("Unauthorized, invalid login details");
             } else {
                 $user = UserAuth::where('username', $request['username'])->firstOrFail();
-
-                $token = $user->createToken('auth_token')->plainTextToken;
-
-                return $this->respondWithSuccess(['access_token' => $token, 'token_type' => 'Bearer']);
+                $employees = Employees::where('user_id', $user->user_id)->first();
+                if (!empty($employees)) {
+                    $date_now = Carbon::now();
+                    $work_schedules = WorkSchedules::where(['employee_id' => $employees->employee_id, 'working_date' => $date_now])->first();
+                    if (!empty($work_schedules)) {
+                        $token = $user->createToken('auth_token')->plainTextToken;
+                        return $this->respondWithSuccess(['access_token' => $token, 'token_type' => 'Bearer']);
+                    } else {
+                        return $this->respondError("It's not your shift");
+                    }
+                } else {
+                    $token = $user->createToken('auth_token')->plainTextToken;
+                    return $this->respondWithSuccess(['access_token' => $token, 'token_type' => 'Bearer']);
+                }
             }
         }
     }
